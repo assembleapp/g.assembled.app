e4289faa8e0d4bd0805a52f4fa3ebf9980f5be58
grace <grace.c.youngblood@gmail.com>
2021-11-24 19:27:53 -0500

    Add a markdown helper, recommended online.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,9 @@
 require 'redcarpet'
 
+
 class PagesController < ApplicationController
+  include MarkdownHelper
+
   def index
-    rendered = Redcarpet::Markdown.
-      new(Redcarpet::Render::HTML, autolink: true, tables: true).
-      render(File.read("source/index.md"))
-    render html: rendered, layout: true
   end
 end
